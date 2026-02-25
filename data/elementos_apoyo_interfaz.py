2b333b7512d8dc46e58dea3efb52ffaab390cac4
Diego Jose Garcia Callejas <d.j.garciacallejas@gmail.com>
2026-02-25 17:18:50 +0100

    Update

---
+++ data/elementos_apoyo_interfaz.py
@@ -0,0 +1,47 @@
+
+#Diccionario para poder colocar botones en la interfaz
+station_positions = {
+    "9 de Julio": {"coords": (701, 233.5), "bg_color": "#008067"},
+    "Alberti": {"coords": (472.5, 338), "bg_color": "#00ADDB"},
+    "Avenida de Mayo": {"coords": (687, 338), "bg_color": "#0168B3"},
+    "Belgrano": {"coords": (710.5, 432), "bg_color": "#6C2180"},
+    "Bolívar": {"coords": (775, 373.5), "bg_color": "#6C2180"},
+    "Callao B": {"coords": (562.5, 213), "bg_color": "#EE1B2E"},
+    "Callao D": {"coords": (564.5, 121.5), "bg_color": "#008067"},
+    "Carlos Pellegrini": {"coords": (701, 213), "bg_color": "#EE1B2E"},
+    "Catedral": {"coords": (775, 302), "bg_color": "#008067"},
+    "Congreso": {"coords": (565.5, 338), "bg_color": "#00ADDB"},
+    "Constitución": {"coords": (687, 750.5), "bg_color": "#0168B3"},
+    "Diagonal Norte": {"coords": (724, 257), "bg_color": "#0168B3"},
+    "Entre Ríos": {"coords": (566, 620.5), "bg_color": "#6C2180"},
+    "Facultad de Medicina": {"coords": (488, 121.5), "bg_color": "#008067"},
+    "Florida": {"coords": (774, 213), "bg_color": "#EE1B2E"},
+    "General San Martín": {"coords": (778.5, 85), "bg_color": "#0168B3"},
+    "Independencia C": {"coords": (687, 525), "bg_color": "#0168B3"},
+    "Independencia E": {"coords": (641, 525), "bg_color": "#6C2180"},
+    "Lavalle": {"coords": (740, 182), "bg_color": "#0168B3"},
+    "Leandro N.Alem": {"coords": (821.5, 213), "bg_color": "#EE1B2E"},
+    "Lima": {"coords": (657, 338), "bg_color": "#00ADDB"},
+    "Moreno": {"coords": (687, 402), "bg_color": "#0168B3"},
+    "Pasco": {"coords": (518, 338), "bg_color": "#00ADDB"},
+    "Pasteur": {"coords": (497, 213), "bg_color": "#EE1B2E"},
+    "Perú": {"coords": (764, 338), "bg_color": "#00ADDB"},
+    "Pichincha": {"coords": (495, 620.5), "bg_color": "#6C2180"},
+    "Piedras": {"coords": (730, 338), "bg_color": "#00ADDB"},
+    "Plaza de Mayo": {"coords": (814.5, 338), "bg_color": "#00ADDB"},
+    "Retiro": {"coords": (825, 43), "bg_color": "#0168B3"},
+    "Sáenz Peña": {"coords": (613, 338), "bg_color": "#00ADDB"},
+    "San José": {"coords": (615, 620.5), "bg_color": "#6C2180"},
+    "San Juan": {"coords": (687, 620), "bg_color": "#0168B3"},
+    "Tribunales": {"coords": (630, 168.5), "bg_color": "#008067"},
+    "Uruguay": {"coords": (612.5, 213), "bg_color": "#EE1B2E"},
+}
+
+
+#Nombre de las estaciones
+estaciones= ["9 de Julio", "Alberti", "Avenida de Mayo", "Belgrano", "Bolívar", "Callao B", "Callao D", "Carlos Pellegrini", "Catedral", "Congreso", 
+"Constitución","Diagonal Norte", "Entre Ríos", "Facultad de Medicina", "Florida", "General San Martín", "Independencia C", "Independencia E", 
+"Lavalle", "Leandro N.Alem", "Lima", "Moreno","Pasco", "Pasteur", "Perú", "Pichincha", "Piedras", "Plaza de Mayo", "Retiro", "Sáenz Peña","San José", 
+"San Juan", "Tribunales", "Uruguay"]
+
+
